31e2d0ee0c3ed6ae8e3969b8b8dcb6d9201a7b9f
kamini <kaminichouhan083@gmail.com>
2025-12-29 15:12:49 +0530

    responsive navbar

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,45 +1,50 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
-import  CartContext from "../context/CartContext";
+import { useContext, useState } from "react";
+import CartContext from "../context/CartContext";
+
 
 function Header() {
   const { cart } = useContext(CartContext);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header className="header">
-      <h2>Hungry Point</h2>
+      <h2 className="logo-font">Hungry Point</h2>
 
       <nav className="navbar">
-        <ul className="nav-links">
+        <ul className={`nav-links ${isOpen ? "active" : ""}`}>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
           </li>
-
           <li>
-            <Link to="/menu">Menu</Link>
+            <Link to="/menu" onClick={() => setIsOpen(false)}>Menu</Link>
           </li>
-
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about" onClick={() => setIsOpen(false)}>About</Link>
           </li>
-
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
           </li>
-
           <li>
-            <Link to="/cart">
+            <Link to="/cart" onClick={() => setIsOpen(false)}>
               🛒 Cart <strong>({cart.length})</strong>
             </Link>
           </li>
         </ul>
+
+        {/* Hamburger Icon */}
+        <div className="hamburger" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </nav>
     </header>
   );
 }
 
 export default Header;
-
-
-
-
